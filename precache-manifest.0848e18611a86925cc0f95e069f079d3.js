f8e1d04e68f20cc6ec1a7e1859f71c6dfbeda022
Aliaksandr Razhachkin <bubonik.san1993@gmail.com>
2020-01-09 02:43:28 +0300

    init

---
+++ precache-manifest.0848e18611a86925cc0f95e069f079d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91efcac5834ac294cd57d43a9dd66a0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23277b4f5bcfd8b738a3",
+    "url": "./static/css/main.afda030d.chunk.css"
+  },
+  {
+    "revision": "c6bf809cd9b97f5e25a7",
+    "url": "./static/js/2.cedcbcb3.chunk.js"
+  },
+  {
+    "revision": "55a85427658879d3739ff9d31aa1742e",
+    "url": "./static/js/2.cedcbcb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23277b4f5bcfd8b738a3",
+    "url": "./static/js/main.8c5eb97c.chunk.js"
+  },
+  {
+    "revision": "d6337e465e57e459f23d",
+    "url": "./static/js/runtime-main.d5c3cebc.js"
+  },
+  {
+    "revision": "0a956a2579c0fd0f6e94cc9cb9c3a701",
+    "url": "./static/media/16799.0a956a25.ttf"
+  },
+  {
+    "revision": "2393683acb01cccca688e0245cf9de67",
+    "url": "./static/media/piskel.2393683a.png"
+  },
+  {
+    "revision": "4317ed28bf966a9b927dec8f40216395",
+    "url": "./static/media/portal.4317ed28.gif"
+  }
+]);
